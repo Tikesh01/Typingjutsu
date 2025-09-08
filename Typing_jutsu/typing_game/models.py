aec0f3c25b4265265db0093e4e88b84b44a630d7
Tikesh sahu <ts2665857@gmail.com>
2025-09-08 23:36:22 +0530

    fa

--- Typing_jutsu/typing_game/models.py
+++ Typing_jutsu/typing_game/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class user(models.Model):
+    id=models.AutoField(primary_key=True, unique=True,)
+    name = models.CharField(max_length=30)
+    email = models.EmailField(max_length=40, unique=True)
+    mobile_num = models.BigIntegerField(max_length=10)
+    role = models.CharField(max_length=10)
+    password = models.CharField(max_length=8)
