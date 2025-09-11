c1d707835ea6fdf640a5e85e33a014041badaf4f
Tikesh sahu <ts2665857@gmail.com>
2025-09-11 15:09:01 +0530

    cv

--- Typing_jutsu/build.sh
+++ Typing_jutsu/build.sh
@@ -4,5 +4,5 @@ set -o errexit
 
 pip install -r requirements.txt
 
-python Typing_jutsu/manage.py collectstatic --no-input
-python Typing_jutsu/manage.py migrate
+python manage.py collectstatic --no-input
+python manage.py migrate
