07de4e0bbff433fbee205cc25384497744a51010
Hitesh sahu <hiteshksahu2006@gmail.com>
2025-09-14 13:08:07 +0530

    Fix static files configuration for deployment

--- Typing_jutsu/build.sh
+++ Typing_jutsu/build.sh
@@ -2,7 +2,14 @@
 # exit on error
 set -o errexit
 
+# Install Python dependencies
 pip install -r requirements.txt
 
-python manage.py collectstatic --no-input
+# Create staticfiles directory if it doesn't exist
+mkdir -p staticfiles
+
+# Collect static files
+python manage.py collectstatic --no-input --clear
+
+# Run database migrations
 python manage.py migrate
